42c915f7622bb11cdff11653a0227eb5bc250263
Tirqia <atirkia1@gmail.com>
2025-01-03 12:47:16 +0400

    first commit

---
+++ LEVEL02/HOMEWORK/varibles.py
@@ -0,0 +1,13 @@
+proposal = "I am"
+
+name = "akeko"
+surname = "tirkia"
+live = "zugdidi"
+age = "16"
+height = "1,74"
+hobby = "programming"
+
+print(proposal + " " + name + " " + surname + " " +
+      live + " " + age + " " + height + " " + hobby)
+
+
---
+++ LEVEL08/HOMEWORK/input.py
@@ -0,0 +1,9 @@
+user_input = input("შეიყვანეთ მონაცემთა ტიპი: ")
+
+print(type(user_input))
+
+# ვქმნით ცვლადს და ვანიჭებთ string მნიშვნელობას
+my_variable = "გამარჯობა, მსოფლიო!"
+
+# ვბეჭდავთ ცვლადის მონაცემთა ტიპს
+print(f"<class {type(my_variable).__name__}>")
